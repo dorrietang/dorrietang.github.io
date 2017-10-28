09a6ac396aee59efefaa0c0d9a310b7aa181dcc3
Dorrie Tang <dorrie.tang@gmail.com>
2017-10-28 11:02:05 -0400

    Version created at DivHacks 2017

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+	$('h1').on('click', function() {
+	  $(this).css({
+	    "color": "#42cbad",
+	    "font-weight": "700",
+	    "line-height": "40px",
+	  });
+	});
+
+	$('h3').on('mouseover', function() {
+	  /*$(this).animate({
+	    
+	  }, 1000);*/
+	  $(this).css({
+	    "background-color": "rgb(200,200,200)"
+	  });
+	});
+
+	$('h3').on('mouseleave', function() {
+	  $(this).css({
+	    "background-color": "rgba(250,250,250, 0.9)"
+	  });
+	});
+});
